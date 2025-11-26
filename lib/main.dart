5da77c35738c14da1975689586b29acc105158a3
Mohammad Baajour <m.i_baajour@outlook.com>
2025-11-26 20:36:38 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Sleep Cycle Calculator',
+      title: 'Sleep Advisor',
       home: HomePage(),
     );
   }
